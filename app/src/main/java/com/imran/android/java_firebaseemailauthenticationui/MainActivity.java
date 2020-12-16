7ffc380be793419a2414fd955600768293630264
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-17 00:55:40 +0600

    main activity

--- app/src/main/java/com/imran/android/java_firebaseemailauthenticationui/MainActivity.java
+++ app/src/main/java/com/imran/android/java_firebaseemailauthenticationui/MainActivity.java
@@ -2,24 +2,31 @@ package com.imran.android.java_firebaseemailauthenticationui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = "EmailUI_Auth";
+    TextView textSigningStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textSigningStatus = findViewById(R.id.textViewSigningStatus);
+
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
         if (firebaseAuth.getCurrentUser() != null) {
             // already signed in
+            textSigningStatus.setText("Signed In");
         } else {
             // not signed in
+            textSigningStatus.setText("Signed Out");
         }
     }
 
