adcacc72658d0d84b7dbe665f50cdbb874e394bd
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-18 21:39:26 +0600

    main activity

--- app/src/main/java/com/imran/android/java_firebaseemailauthenticationui/MainActivity.java
+++ app/src/main/java/com/imran/android/java_firebaseemailauthenticationui/MainActivity.java
@@ -3,10 +3,15 @@ package com.imran.android.java_firebaseemailauthenticationui;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     String TAG = "EmailUI_Auth";
@@ -35,7 +40,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signIn(View view) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
+        if (firebaseAuth.getCurrentUser() != null) {
+            // already signed in
+            textSigningStatus.setText("Signed In");
+            Toast.makeText(this,
+                    "User already signed in, must sign out first",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            // Choose authentication builder
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build()
+            );
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setDefaultProvider(providers.get(0))
+                    .build(),
+                   1234
+            );
+        }
     }
 
     public void signOut(View view) {
